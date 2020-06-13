99f984c19f033ba2527087e4923c0164478c7576
Gustavo Bertolino <gustavobertolino@gmail.com>
2020-06-13 20:46:32 -0300

    Adjust install script

--- install.sh
+++ install.sh
@@ -32,3 +32,106 @@ if ! [ $fsok = 'y' ] && ! [ $fsok = 'Y' ]
     echo "Exiting! Edit the script and run it again to continue..."
     exit 0
 fi
+
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV}
+  o # clear the in memory partition table
+  n # new partition
+  p # primary partition
+  1 # partition number 1
+    # default - start at beginning of disk
+  +512M # 512 MB boot parttion
+  n # new partition
+  p # primary partition
+  2 # partion number 2
+    # default, start immediately after preceding partition
+  +9G
+  n # new partition
+  p # primary partition
+  3 # partion number 3
+    # default, start immediately after preceding partition
+    # default, extend partition to end of disk
+  +1G
+  t # change partition type to swap
+  3 # partition to be swap
+  82 # code for swap
+  n #new partition
+  p # primary partition
+  4 # partition number 4
+    # default - start at beginning of disk
+    # default - extend to the end of the disk
+  a # make a partition bootable
+  1 # bootable partition is partition 1 -- /dev/sda1
+  p # print the in-memory partition table
+  w # write the partition table
+  q # and we're done
+EOF
+
+# Format the partitions
+mkfs.fat -F32 /dev/sda1
+mkfs.ext4 /dev/sda2
+mkfs.ext4 /dev/sda4
+
+mkswap /dev/sda3
+swapon /dev/sda3
+
+# Mount the partitions
+mount /dev/sda2 /mnt
+
+mkdir -p /mnt/boot/efi
+mount /dev/sda1 /mnt/boot/efi
+
+mkdir -p /mnt/home
+mount /dev/sda4 /mnt/home
+
+#Install arch linux packages
+pacstrap /mnt base linux sudo vim grub efibootmgr networkmanager
+
+#Generate fstab
+genfstab -U -p /mnt >> /mnt/etc/fstab
+
+#Enter the installed OS
+arch-chroot /mnt
+
+#Set local time
+ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+
+#Sync time with system clock time
+hwclock --systohc
+
+#Edit locale and uncomment the en_US.UTF-8
+lang=en_US.UTF-8; 
+sed -i "/^#$lang/ c$lang" /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" /etc/locale.conf
+
+#Set keyboard layout for installed OS
+echo "KEYMAP=br-abnt2" /etc/vconsole.conf
+
+#Set hostname
+read -p "Give a hostname:" hostname
+echo $hostname /etc/hostnames
+echo "127.0.1.1	${hostname}.localdomain $hostname"
+
+#Enable networkmanager
+systemctl enable NetworkManager
+
+#Add root password
+echo "Add root password:"
+passwd
+
+#Install bootloader
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --removable
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Create new user
+useradd -m -G wheel,power,iput,storage,uucp,network $hostname
+sed --in-place 's/^#\s*\(%wheel\s\+ALL=(ALL)\s\+NOPASSWD:\s\+ALL\)/\1/' /etc/sudoers
+
+# Set user password
+echo "Set user password:"
+passwd $hostname
+
+echo "The only thing left is to reboot into the new system."
+echo "Press any key to reboot or Ctrl+C to cancel..."
+read tmpvar
+reboot
