e3888513d9e43ef45275dd25f19657fa0691beb6
Gustavo Bertolino <gustavobertolino@gmail.com>
2020-06-14 18:30:35 -0300

    Adjust wm-i3 font line

--- os-wm-setup.sh
+++ os-wm-setup.sh
@@ -1,3 +1,3 @@
-sudo pacman -S xorg xorg-server xorg-xinit pulseaudio-alsa mesa mesa-libgl virtualbox-guest-utils dmenu dejavu-ttf i3 rxvt-unicode 
+sudo pacman -S xorg xorg-server xorg-xinit pulseaudio-alsa mesa mesa-libgl virtualbox-guest-utils dmenu ttf-linux-libertine i3 rxvt-unicode 
 echo "exec i3" >> ~/.xinitrc
 startx
