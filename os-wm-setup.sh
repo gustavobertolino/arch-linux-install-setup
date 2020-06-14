fea246660d095e6a5d88cdc6dabcd86ede335eba
Gustavo Bertolino <gustavobertolino@gmail.com>
2020-06-14 20:06:21 -0300

    Adjust wm font and terminal lines

--- os-wm-setup.sh
+++ os-wm-setup.sh
@@ -1,3 +1,19 @@
-sudo pacman -S xorg xorg-server xorg-xinit pulseaudio-alsa mesa mesa-libgl virtualbox-guest-utils dmenu ttf-linux-libertine i3 rxvt-unicode 
+#Install i3 wm, fonts and other basic tools
+sudo pacman -S xorg xorg-server xorg-xinit pulseaudio-alsa mesa mesa-libgl virtualbox-guest-utils dmenu ttf-dejavu i3 rxvt-unicode \
+  brightnessctl
 echo "exec i3" >> ~/.xinitrc
+
+#Double-check locale and uncomment the en_US.UTF-8
+LANG_USER=en_US.UTF-8^UTF-8;
+sed -i "/^#$LANG_USER/ c$LANG_USER" /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "setxkbmap -model abnt2 -layout br -variant abnt2" >> ~/.config/i3/config
+
+touch .xResources
+echo "URxvt.font: xft:DejavuSansMono:pixelsize=16" >> .Xresources
+echo "URxvt.background: #000000" >> .Xresources
+echo "URxvt.foreground: #ffffff" >> .Xresources
+
+xrdb -load .Xresources
 startx
