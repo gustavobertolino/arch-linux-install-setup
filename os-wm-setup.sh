c7682e846320d2c786969f88beebd5fdcc619eb8
Gustavo Bertolino <gustavobertolino@gmail.com>
2020-06-14 23:59:15 -0300

    Include aws to install in script

--- os-wm-setup.sh
+++ os-wm-setup.sh
@@ -3,7 +3,7 @@ sudo pacman -S xorg xorg-server xorg-xinit pulseaudio-alsa brightnessctl mesa me
   ttf-dejavu rxvt-unicode unzip \
   brightnessctl lightdm lightdm-gtk-greeter mlocate htop neofetch \ 
   httpie git tig ranger diff-so-fancy clojure erlang elixir ruby ruby-irb rubygems \
-  redis mysql postgresql rabbitmq docker docker-compose emacs
+  redis mysql postgresql rabbitmq docker docker-compose aws-cli emacs
 
 sudo pacman -S gcc python python-pip
 sudo pip install pgcli mycli
