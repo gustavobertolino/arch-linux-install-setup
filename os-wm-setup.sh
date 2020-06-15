4387eda789fa2ab3dc39121a14a37abd8b99a120
Gustavo Bertolino <gustavobertolino@gmail.com>
2020-06-15 13:53:30 -0300

    Adjust os-wm setup i3 lines and reboot

--- os-wm-setup.sh
+++ os-wm-setup.sh
@@ -1,11 +1,11 @@
 #Install i3 wm, fonts and other basic tools
 sudo pacman -S xorg xorg-server xorg-xinit pulseaudio-alsa brightnessctl mesa mesa-libgl virtualbox-guest-utils i3 dmenu shutter \ 
   ttf-dejavu rxvt-unicode unzip \
-  brightnessctl lightdm lightdm-gtk-greeter mlocate htop neofetch \ 
+  brightnessctl lightdm lightdm-gtk-greeter mlocate htop neofetch feh 
 
 echo "Do you want also install developer setup packages? [y=1/N=0]"
 read aws
-if [$aws = 1]; then
+if [$aws == 1]; then
   sudo pacman -S httpie git tig ranger diff-so-fancy clojure erlang elixir ruby ruby-irb rubygems \
   redis mysql postgresql rabbitmq docker docker-compose aws-cli emacs
   
@@ -24,14 +24,26 @@ locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "exec setxkbmap -model abnt2 -layout br -variant abnt2" >> ~/.config/i3/config
 
+#Set documents folder and desktop background
+mkdir -p ~/documents
+IMAGE_URL=https://i.pinimg.com/originals/e9/83/9c/e9839c0bf09bddcce7ac3ce0f2df047c.jpg
+IMAGE_FILE=e9839c0bf09bddcce7ac3ce0f2df047c.jpg
+wget -c $IMAGE_URL
+mv IMAGE_FILE dark-wallpaper.jpg
+mv dark-wallpaper ~/documents
+echo "exec_always feh --bg-scale ~/documents/dark-wallpaper.jpg" >> ~.config/i3/config
+
+#Set terminal font, background and transparency in terminal
 touch .xResources
 echo "URxvt.font: xft:DejavuSansMono:pixelsize=16" >> .Xresources
 echo "URxvt.background: #000000" >> .Xresources
 echo "URxvt.foreground: #ffffff" >> .Xresources
 updatedb
+xrdb -load .Xresources
 
+#Enable display log-in screen (display manager)
 sudo systemctl enable lightdm
 
-xrdb -load .Xresources
-exit
+echo "Run reboot command or Ctrl+C to cancel. After rebooting you will see new i3-wm layout: "
+read aws
 reboot
